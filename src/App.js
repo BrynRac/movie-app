f403a6141ebf9f114b65ae441eb5fc12d7430725
brynrac <brynrace@gmail.com>
2020-07-07 16:31:48 -0700

    Add react-router-dom. Add Home, About, Nav components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+//components
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
-  return <div className="App">
-    <h1>Hello World</h1>
-  </div>;
+  return (
+    <main className="App">
+      <Nav />
+
+      <h1>Hello World</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Nav />
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link, Router } from 'react-router-dom';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="nav-container">
+        <nav>
+          <span>LOGO</span>
+          <ul className="navLinks">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Nav;
