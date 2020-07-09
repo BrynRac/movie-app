93930939e89f43f84d27d2fe6ff4f119c880e781
brynrac <brynrace@gmail.com>
2020-07-09 16:23:14 -0700

    change api links to https

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -8,7 +8,7 @@ export const searchMovie = (text) => (dispatch) => {
 
 export const fetchMovies = (text) => async (dispatch) => {
   const response = await axios.get(
-    `http://www.omdbapi.com/?s=${text}&apikey=${APIKEY}`
+    `https://www.omdbapi.com/?s=${text}&apikey=${APIKEY}`
   );
   try {
     return dispatch({ type: FETCH_MOVIES, payload: response.data });
@@ -19,7 +19,7 @@ export const fetchMovies = (text) => async (dispatch) => {
 
 export const fetchMovie = (id) => async (dispatch) => {
   const response = await axios.get(
-    `http://www.omdbapi.com/?i=${id}&apikey=${APIKEY}`
+    `https://www.omdbapi.com/?i=${id}&apikey=${APIKEY}`
   );
   try {
     return dispatch({ type: FETCH_MOVIE, payload: response.data });
