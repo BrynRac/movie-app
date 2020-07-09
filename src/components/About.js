c036813742d36c5c54ffe48fac664fbf1ffbc64b
brynrac <brynrace@gmail.com>
2020-07-09 02:34:48 -0700

    Complete About component.

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h2>About</h2>
+    <div className="About">
+      <img
+        src="https://media.giphy.com/media/kdiLau77NE9Z8vxGSO/giphy.gif"
+        alt="fun gif"
+      />
+      <div className="about-text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id
+          rutrum dolor, quis finibus felis. Donec ullamcorper enim eros, sit
+          amet maximus risus commodo sed. Duis non sapien facilisis, elementum
+          urna ac, gravida enim. Curabitur ornare lorem mi, quis consectetur
+          libero vulputate eget. Vivamus laoreet nulla ac mi tempus facilisis.
+          Donec laoreet eget neque id semper. Aliquam pulvinar lectus at lectus
+          condimentum, et rutrum ex tempus. Curabitur suscipit sapien sed
+          suscipit porttitor. Maecenas sit amet dolor nunc. Praesent vestibulum
+          nibh ac leo dapibus, non porta enim bibendum. Aenean pretium justo
+          vitae sem posuere luctus ut at metus. Sed sed consequat diam. Mauris
+          eget augue mattis, molestie diam sit amet, porta nisi. Ut ut urna at
+          justo posuere facilisis. Proin tempor ipsum ut dolor pellentesque, sit
+          amet placerat ligula fermentum.
+          <br />
+          <br />
+          Sed ultrices vel massa a blandit. Nam sit amet fermentum felis.
+          Vestibulum viverra sollicitudin fringilla. Donec sit amet cursus
+          neque. Nullam cursus elementum iaculis. Fusce non nunc ac nibh blandit
+          lacinia. Donec venenatis vulputate leo sed rutrum. Curabitur sed
+          congue mauris. Fusce erat nisi, pharetra quis ante vel, ullamcorper
+          hendrerit justo. Nullam consequat felis non dictum dapibus. Sed
+          malesuada, urna sit amet bibendum pretium, nibh felis luctus nibh, sed
+          laoreet velit neque id tortor. Nullam rhoncus sapien nec tortor
+          sollicitudin aliquam.
+        </p>
+      </div>
     </div>
   );
 }
